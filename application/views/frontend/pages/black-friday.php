81e8c7fbb2a74db9a839c9009e52699ae32b2dc5
sharvan <sharvan@fxcareers.com>
2024-11-21 11:11:53 +0530

    banner-commit

--- application/views/frontend/pages/black-friday.php
+++ application/views/frontend/pages/black-friday.php
@@ -1,1 +1,11 @@
-<h1>Test</h1>
+<section class="black-friday-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="head">
+                    <h2>Black Friday Extra Bonus </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
