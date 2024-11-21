63d16d82420855a70563cdaae950c93b1ba04571
sharvan <sharvan@fxcareers.com>
2024-11-21 12:47:51 +0530

    black-friday-complete

--- application/views/frontend/pages/black-friday.php
+++ application/views/frontend/pages/black-friday.php
@@ -1,11 +1,211 @@
-<section class="black-friday-banner">
+<section class="black-friday-banner graident_bg">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-lg-6 align-self-center">
                 <div class="head">
                     <h2>Black Friday Extra Bonus </h2>
+                    <h5>Celebrate Black Friday with a 29% Bonus You Can Withdraw!</h5>
+                    <p>This Black Friday, we’re giving you more reasons to celebrate! Get a 29% Deposit Bonus that’s not
+                        just for trading—it’s withdrawable once you meet the trading requirements.
+                    </p>
+                    <a class="theme-btn" data-bs-toggle="modal" data-bs-target="#FormModal"><i class="fas fa-chart-line"
+                            aria-hidden="true"></i>Participate</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="banner-right">
+                    <img src="images/bf-bonus-banner.png" class="img-fluid" alt="">
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<section class="black-firday-details bg-light">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12 text-center">
+                <h2 class="fw-bold mb-5 text-black">
+                    Black Friday <span class="pink-text">
+                        Promotion Details
+                    </span>
+                </h2>
+
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-4">
+                <div class="details-box">
+                    <div class="logo">
+                        <img src="images/black-friday-discount.svg" alt="">
+                    </div>
+                    <h3>Bonus Amount</h3>
+                    <p>29% on every deposit made during the promotion period.</p>
+
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="details-box bg-pink">
+                    <div class="logo">
+                        <img src="images/black-friday-promotion-period.svg" alt="">
+                    </div>
+                    <h3>Promotion Period</h3>
+                    <p> November 25th - 31st.</p>
+
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="details-box">
+                    <div class="logo">
+                        <img src="images/black-friday-eligibility.svg" alt="">
+                    </div>
+                    <h3>Eligibility</h3>
+                    <p>Available to all traders (new and existing).
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="black_friday_trem_contidion graident_bg">
+    <div class="container">
+        <div class="row ">
+            <div class="col-12">
+                <div class="head text-center  text-light ">
+
+                    <h2 class="mt-5">
+                        Terms & Conditions
+                    </h2>
+                    <a class="theme-btn px-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Terms & Conditions</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">  Terms & Conditions</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="list_wraper">
+        <ol class="p-4 ol-list ">
+            <li>The contest runs from November 1st, 2024 to December 20th, 2024.</li>
+            <li>Rewards will be distributed on 25th December, following verification of eligibility criteria.</li>
+            <li>Rewards are determined by Net Deposits and the number of Lots traded during the contest period.</li>
+            <li>Deposits must be made within the contest period to be eligible.</li>
+            <li>The promotion is not available for ECN and Raw account types.</li>
+            <li>Sub-accounts are not considered for the "new open account" requirement.</li>
+            <li>Internal transfers between accounts are not eligible and do not count towards the deposit requirement.
+            </li>
+            <li>Participants must comply with NXG Markets' standard trading conditions on all referred accounts. Any
+                trades that violate these conditions will be excluded from the reward calculation.</li>
+            <li>Rewards will only be given to clients with verified and active accounts.</li>
+            <li>NXG Markets reserves the right to modify or cancel the contest, its rewards, or its terms at any time
+                without prior notice.</li>
+            <li>NXG Markets may exclude participants found to be engaging in fraudulent activities or manipulative
+                trading practices from the contest.</li>
+            <li>Any account that becomes inactive or is closed during the contest period will be disqualified from
+                reward eligibility.</li>
+            <li>The contest is subject to all applicable NXG Markets’ rules and regulations.</li>
+
+        </ol>
+    </div>
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-8">
+    
+</div>
+
+<style>
+    section {
+        padding: 100px 30px;
+    }
+
+    .black-friday-banner {
+        padding: 100px 0px;
+    }
+
+    .graident_bg {
+        background-image: radial-gradient(#94204c, rgb(0, 0, 0));
+
+    }
+
+    .black-friday-banner .head h2 {
+        font-size: 55px;
+        color: white !important;
+    }
+
+    .black-friday-banner .head h5 {
+        color: white !important;
+    }
+
+    .black-friday-banner .head p {
+        padding: 20px 0px;
+        color: white;
+    }
+
+    .pink-text {
+        color: var(--purple)
+    }
+
+    .theme-btn {
+        position: relative;
+        display: inline-block;
+        overflow: hidden;
+        font-size: 17px;
+        padding: 12px 60px 11px 30px;
+        background: #fff;
+        line-height: 25px;
+        color: var(--purple);
+        border: 2px solid var(--purple);
+        font-weight: 500;
+        border-radius: 30px;
+        text-transform: capitalize;
+        transition: all 500ms ease;
+        cursor: pointer;
+        z-index: 1;
+        text-decoration: none;
+    }
+
+    .details-box {
+        /* border: 2px solid black; */
+        height: 100%;
+        padding: 20px;
+        border-radius: 5px;
+
+        box-shadow: 0px 0px 5px 5px rgba(181, 44, 97, 0.15);
+    }
+
+    .bg-pink {
+        color: white;
+        background-color: var(--purple);
+        transform: scale(1.15);
+    }
+
+    .details-box h3 {
+        margin: 10px 0px;
+    }
+
+    .black_friday_trem_contidion {
+        position: relative;
+    }
+
+ 
+</style>
